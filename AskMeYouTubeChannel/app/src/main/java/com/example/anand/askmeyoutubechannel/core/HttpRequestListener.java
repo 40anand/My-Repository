31b4ba8b282898fbe9de3076411a8f5840476020
40anand <aditya.anand2020@gmail.com>
2015-08-03 23:30:24 +0530

    YouTubeChannel
    To Play Videos of Any YouTube Channel

--- AskMeYouTubeChannel/app/src/main/java/com/example/anand/askmeyoutubechannel/core/HttpRequestListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.anand.askmeyoutubechannel.core;
-
-public interface HttpRequestListener {
-    void onFirstPageLoadSuccess(String result);
-
-    void onPageLoadSuccess(String result);
-
-    void onPageLoadFailure(boolean isFirstPage);
-}
--- AskMeYouTubeChannel/app/src/main/java/com/example/anand/askmeyoutubechannel/core/LoadDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.anand.askmeyoutubechannel.core;
-
-/**
- * Created by Anand on 11-07-2015.
- */
-public interface LoadDataListener {
-    void onLoadRequest();
-}
